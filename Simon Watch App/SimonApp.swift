b6a8e95b533247c4a53294021e32a61c02aa3341
YossiBenZaken <yossibz@gmail.com>
2022-10-10 16:06:35 +0300

    Initial Commit

---
+++ Simon Watch App/SimonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimonApp.swift
+//  Simon Watch App
+//
+//  Created by Yosef Ben Zaken on 10/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Simon_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Simon/SimonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimonApp.swift
+//  Simon
+//
+//  Created by Yosef Ben Zaken on 10/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct SimonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
